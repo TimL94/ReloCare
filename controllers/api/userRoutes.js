fc2b28e3499977afdcc7466f99b4cc7119573e3b
afry72 <ajfrye72@gmail.com>
2024-04-08 19:50:49 -0400

    added some bootstrap changes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -60,7 +60,7 @@ router.post('/login', async (req, res) => {
 });
 
 router.post('/logout', (req, res) => {
-  if (req.session.logged_in) {
+  if (req.session.loggedIn) {
     // Remove the session variables
     req.session.destroy(() => {
       res.status(204).end();
